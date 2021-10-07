8f295b28003309c576b02ba167fe80ae2165350f
Noel Jimenez <noeljimenez.400@gmail.com>
2021-10-06 23:24:51 -0400

    commit inicial

---
+++ source/New folder/MantenimientoEstaciones/MantenimientoEstaciones.Web/Models/Estacion.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MantenimientoEstaciones.Web.Models
+{
+    public class Estacion
+    {
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Tipo { get; set; }
+        public decimal Latitud { get; set; }
+        public decimal Longitud { get; set; }
+        public string Altura { get; set; }
+        public DateTime? FechaInstalacion { get; set; }
+        public string Fotos { get; set; }
+        public string Contactos { get; set; }
+        public string Equipos { get; set; }
+        public string TipoTx { get; set; }
+
+        public List<Mantenimiento> Mantenimientos { get; set; }
+        public int Index { get; set; }
+    }
+
+    public class Mantenimiento 
+    {
+    public DateTime Fecha { get; set; }
+        public decimal Estacion { get; set; }
+        public string FallaTecnica { get; set; }
+        public string PosibleCausa { get; set; }
+        public string Observaciones { get; set; }
+        public bool Solucionado { get; set; }
+        public DateTime? FechaSolucion { get; set; }
+        public string Solucion { get; set; }
+
+        public int Index { get; set; }
+    }
+}
---
+++ source/New folder/MantenimientoEstaciones/MantenimientoEstaciones.Web/Service/Dataservice.cs
@@ -0,0 +1,80 @@
+﻿using MantenimientoEstaciones.Web.Models;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MantenimientoEstaciones.Web.Service
+{
+    public class Dataservice
+        
+    {
+        IWebHostEnvironment hostEnvironment;
+        public Dataservice(IWebHostEnvironment webHost) 
+        {
+            this.hostEnvironment = webHost;
+        }
+
+        public List<Estacion> ObtenerEstaciones() 
+        {
+            List<Estacion> resultado = new List<Estacion>();
+
+            return resultado;
+        }
+
+        public Estacion ObtenerEstacionPorId( string id)
+        {
+           Estacion resultado = new Estacion();
+
+            return resultado;
+        }
+
+        public List<Mantenimiento> ObtenerMantenimientosDeEstacion(string id)
+        {
+            List<Mantenimiento> resultado = new List<Mantenimiento>();
+
+            return resultado;
+        }
+
+        public Estacion GuardarEstacion(Estacion estacion) 
+        {
+            Estacion resultado = new Estacion();
+
+            return resultado;
+        }
+
+        public Mantenimiento GuardarMantenimiento(Mantenimiento mantenimiento)
+        {
+            Mantenimiento resultado = new Mantenimiento();
+
+            return resultado;
+        }
+
+        public Estacion ActualizarEstacion(Estacion estacion)
+        {
+            Estacion resultado = new Estacion();
+
+            return resultado;
+        }
+
+        public Mantenimiento ActualizarMantenimiento(Mantenimiento mantenimiento)
+        {
+            Mantenimiento resultado = new Mantenimiento();
+
+            return resultado;
+        }
+
+        public long BorrarMantenimiento(Mantenimiento mantenimiento) 
+        {
+            
+            return -1;
+        }
+
+        public long BorrarEstacion(Estacion estacion)
+        {
+
+            return -1;
+        }
+    }
+}
